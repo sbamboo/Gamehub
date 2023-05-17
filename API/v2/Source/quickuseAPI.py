f092182373d55dd4cf722d6210dcef3cf790dc87
StalkedBamboo <85576459+sbamboo@users.noreply.github.com>
2023-05-18 00:17:05 +0200

    update quickuseAPI

--- API/v2/Source/quickuseAPI.py
+++ API/v2/Source/quickuseAPI.py
@@ -1,6 +1,10 @@
 from gamehubAPI import saveDict,loadDict,scoreboardConnector,gamehub_scoreboardFunc
 from libs.libfilesys import filesys as fs
-import base64,json,os,subprocess;exec(base64.b64decode("aW1wb3J0IGdldHBhc3MNCmltcG9ydCBzb2NrZXQNCmltcG9ydCBkYXRldGltZQ0KaW1wb3J0IGhhc2hsaWINCg0KZGVmIGVudHJvcHlHZW5lcmF0b3IoKSAtPiBzdHI6DQogICAgaG9zdCA9IHNvY2tldC5nZXRob3N0bmFtZSgpDQogICAgZW50cm9weSA9IGhhc2hsaWIuc2hhMjU2KGYie2dldHBhc3MuZ2V0dXNlcigpfS17aG9zdH0te3NvY2tldC5nZXRob3N0YnluYW1lKGhvc3QpfS17ZGF0ZXRpbWUuZGF0ZS50b2RheSgpLnN0cmZ0aW1lKCcleS0lbS0lZCcpfS1HYW1laHViRW50cm9weVZhbHVlX1RvQnJlYWtUaGlzVmFsdWVPclVzZUl0QW55d2hlcmVJdFdhc05vdE9yaWdpbmFseVVzZWRJc0FCcmVha2FnZU9mVGhlR2FtZWh1YkxpY2Vuc2VBbmRUT1MiLmVuY29kZSgpKS5oZXhkaWdlc3QoKQ0KICAgIGhvc3QgPSBOb25lDQogICAgcmV0dXJuIGVudHJvcHk=").decode("utf-8"))
+from libs.libRSA import decrypt_string
+import base64,json,os,subprocess,getpass,socket
+exec(base64.b64decode("aW1wb3J0IGdldHBhc3MKaW1wb3J0IHNvY2tldAppbXBvcnQgZGF0ZXRpbWUKaW1wb3J0IGhhc2hsaWIKCmRlZiBlbnRyb3B5R2VuZXJhdG9yKCkgLT4gc3RyOgogICAgdXNlciA9IGdldHBhc3MuZ2V0dXNlcigpCiAgICBob3N0ID0gc29ja2V0LmdldGhvc3RuYW1lKCkKICAgIGlwX2FkZHJlc3MgPSBzb2NrZXQuZ2V0aG9zdGJ5bmFtZShob3N0KQogICAgZGF0ZSA9IGRhdGV0aW1lLmRhdGUudG9kYXkoKS5zdHJmdGltZSgnJXktJW0tJWQnKQogICAgZW50cm9weV9zdHIgPSBmJ3t1c2VyfS17aG9zdH0te2lwX2FkZHJlc3N9LXtkYXRlfScKICAgIGVudHJvcHkgPSBoYXNobGliLnNoYTI1NihlbnRyb3B5X3N0ci5lbmNvZGUoKSkuaGV4ZGlnZXN0KCkKICAgIHJldHVybiBlbnRyb3B5CgpkZWYgZ2V0U2FmZUNvbmZpZ0tleSgpIC0+IHN0cjoKICAgIHJldHVybiAnJyctLS0tLUJFR0lOIFBSSVZBVEUgS0VZLS0tLS0KTUlJRXZRSUJBREFOQmdrcWhraUc5dzBCQVFFRkFBU0NCS2N3Z2dTakFnRUFBb0lCQVFDbHErV0cwRVhsM3I5Twp1MDNnQmx5ejk5dUJScXExR1Z0R01PL3M3c2ZpSjZPeVU1UkVURmJtWDlZMkxpZVdiNFVybU5SNlBRVkJRNEtWCnp5UDRudVNxL3MvSFFwZHdmZ2ZsdU1xL0t2dW1LOE5lWEF5N2ZpSm9SbTRzc0NESTd4NWIvVUxhYmYwSk1OTGwKN2tzQ1FBMVFPNmJDcFVLMDE5VDg1VFdzcktwdmNGeWc2WWFOajgvaTFUSXB3TnVqR1ZzeW1nYUNPUEtjdmF5OQpxR1FMcXordFQydEdMclA0UElDTjJqNHpmWlZXLytkR2dFOFgzWHN4WG5PVjF6RFFYZjI2dlRyL2kxTHFHTzZEClcza2FwYzdXS2ZuV0lJQW8ySCtGb3pWMUFoY1RtQVJDTjI1d25nL2U2SW5udy9WNzd0L1JHWkRKeFdMSmhnblAKdnJQaWcxR2JBZ01CQUFFQ2dnRUFTN21JQnJpQkVvSmZlRG0xN1QyTE52bUdQQTlVYk1XanlqQUpJb2U4Rmx1ZQovNTRqU1pxSkovRExSV1dRVTdzeXFBeEpwbnZvd0gxK25VSWFnNFFCS2tXaExFZDhWLzlMVmMzQzRtRmZ1QU1OCjREMzcxZUZnRWNDTGtHS0xBYjBHWis2WmxhU3JnWDF2RUlqOGdSRk01SXozZXNXMStWb2o4TnBGeitEMllFdkQKWFpEeUNIRHAyOXhObmV3Y2RzVjhWK3VKbXo0OVAwUU1BWXRxK011T0Q3R21oN2I4WHQxY1RlazlIWXFmVkVQMgpZVUtubWtqejRvemtUU0RhK2V5UHdmVFZVYytmQzdmWVNpK2srdWpObzFOU0tOWTcwY2w3c01iaUhEUzBGeTVzCnBYbTVjV2xMemFkQjJYejVjN2Z0SnRONUhaQTBsSEliMUJUb3NMbXNpUUtCZ1FEQVNFVGlVbGJ2eDM2MDBUR3kKd2FwUyszd3llSUtXYUhJQ1E1QmMrZDNxNjNvS3orMVF5a093RllsVFBRTlVPOUlGS3pXWTUvaENSWVdhQWF4dgpIQnhPWitsbUs5VXlwenZyc1M2OFhmQVVNL1Q3Rlk0NitnWTlCSGVFeTdZVW5xeUxMT1VzaytKTmRwanF1SWlJCk9jblI5eDNMTjduRE9NSHJNT3J0WldlbE13S0JnUURja2l3V0hjNm9wU1pBZXJVR0ZRNXJJS3ZqYkRHUVZidk0KMFh0UzFSWXpLam00M2daMEVuRjVXR25ZcDZTU0xMRVk4SzkzVFVKTzdWN0RUSldVdFAzQVJqODQ4VGlubkNoQgpHWXNGQ2tyYlEvVXJYRzRkY1pzV1FXRGJGQ1FHU1V2c3VvODg3eTZtYnJxQzBYVlJacWlhbkZUZFNpVWo5WUFsCmhpTERoOWZSK1FLQmdDTWFEbGt5cHVSSEN2NS9ZZzg4QTVmNmlRVzlzams2Ly9VaDJHemd3SDV1Vm0wNjRCdnIKa01mSVpyVm5ZZ0F5bTNpT0ZzNi9LamNPOGdEWFpWOHpSb2VadUtZS1FuVm95aXVRd1BOcVFyV3RkbitQdzlOSQphWE1pS1o5NGdOanF6cHpwcVR5bUVwNEpsSWprL3llL1JQU3JwQ2pCRjR4b0JCNm5ZM3ZMRTB5NUFvR0JBSUdnCk9kNkpoL0VlbFh0aFljK0FRbWY4M0dlY0p3aXZDZDVWNGdjTkNhM3FDK0EwUTFDbG9pQnhNRXRPUW01UmE0YS8KdEM4RnJZbGJBTXovemd5RnpYYlpFY0N5S3R4OTdqNUw5Nkp2cVF4eFJMMUY1Y1RTQmhXdk9HK254NEFXUlZPTApWREM0VkE4bGxlRFpuZnZIdkNDWTdWcmJmelpCeWh6RFZ0ellrYUpwQW9HQUQzYTJFOUFVUjRyQjJzNjF6NHFMCjZaM05WTzVZWkZHVTFsY29pbXl0bGJQMVJjcWVsUnpWTWFBWmdFNndpUFhLc1JoUWtOMm9GZGswSnpOZWhoK2YKbS9uMjIyVEdPSkY3MENrNU8rWklyeERTWnlJUldnSmlRaVZ0THpGZDdFbFRBb0JqeTd3bUVkWndWamgrVUx1bgpMWVZ5blY0RnJtbUVUa0VBdzU0ZUhldz0KLS0tLS1FTkQgUFJJVkFURSBLRVktLS0tLScnJw==").decode("utf-8"))
+
+parentDir = os.path.dirname(__file__)
 
 # Function to handle userData
 def gamehub_userData(
@@ -105,6 +109,18 @@ def gamehub_singleSave_score(
         _jsonData = json.loads( {_dict["user"] : _dict["data"]} )
     gamehub_scoreboardFunc(encType=encType,manager=manager,apiKey=apiKey,encKey=encKey,managerFile=managerFile,ignoreManFormat=ignoreManFormat,_scoreboard=_dict["scoreboard"], jsonData=_jsonData, append=True)
 
+# Function to handle apiConfigs
+def getAPIConfig(apiConfPath=str()) -> dict:
+    secureFileEndings = ["sconf","sconfig","secConf","secConfig","ghsc","gh_sconf","gh_sconfig"]
+    # Get content
+    content = open(apiConfPath,'r').read()
+    # Check if secure
+    if (os.path.basename(apiConfPath).split("."))[-1] in secureFileEndings:
+        #DecryptContent
+        content = decrypt_string(content,getSafeConfigKey())
+    # Return content
+    return json.loads(content)
+
 # Function to prep a file for the saveService
 def saveServicePrep(linkedFile=str(),doEncrypt=True, scoreboard=str(),user=str(),data=dict()):
     _path = os.path.dirname(linkedFile)
@@ -121,7 +137,7 @@ def saveServiceFunction(apiConfPath=str(),linkedFile=str(),exitFile=str(),doEncr
     verbose: Boolean flag, set to False to disable the function writing out what it is doing.
     simpleScore: Boolean flag, if set to True the function will handle scores (Only update if newer) Data must be {"score":<intValue>}
     '''
-    apiConf = json.loads(open(apiConfPath,'r').read())
+    apiConf = json.loads( getAPIConfig(apiConfPath) )
     previousContent = str()
     # Init scoreboard connector
     conn = scoreboardConnector(encryptionType=apiConf["encryptionType"], storageType=apiConf["storageType"], key=apiConf["apiKey"], kryptographyKey=apiConf["encKey"], managersFile=apiConf["managersFile"], ignoreManagerFormat=apiConf["ignoreManagerFormat"])
